978dbcd4f8cf04c92a50dd49dced1ddd24aa670d
Aditya Pandey <addy@example.com>
2024-07-10 23:45:25 +0530

    learn fragments in react

--- 01basic/src/App.jsx
+++ 01basic/src/App.jsx
@@ -2,8 +2,11 @@
 import Chai from './chai';
 function App() {
 
-  return (
-<Chai></Chai>  )
+  return ( <>
+    <h1>Title</h1>
+    <p>This is a paragraph.</p>
+
+<Chai></Chai>  </>  )
 }
 
 export default App;
